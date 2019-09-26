fb39a226e16547d06ee8f25fa3c5122b02e279f7
jonathan <jmitchell1991@gmail.com>
2019-09-26 08:51:09 -0700

    [feat] - updates to make triangle instead of rectangle

--- dependencies.js
+++ dependencies.js
@@ -36,8 +36,8 @@ window.Minimal_Shape = window.classes.Minimal_Shape =
 class Minimal_Shape extends Vertex_Buffer    // The simplest possible Shape – one triangle.  It has 3 vertices, each
 { constructor()                              // containing two values: a 3D position and a color.
     { super( "positions", "colors" );                       // Name the values we'll define per each vertex.
-      this.positions = [ Vec.of(0,0,0), Vec.of(1,0,0), Vec.of(0,1,0), Vec.of(0,1,0), Vec.of(1,0,0), Vec.of(1,1,0)];   // Describe the where the points of a triangle are in space.
-      this.colors    = [ Color.of(1,0,0,1), Color.of(0,1,0,1), Color.of(0,0,1,1), Color.of(0,0,1,1), Color.of(0,1,0,1), Color.of(1,1,0,1) ];   // Besides a position, vertices also have a color.
+      this.positions = [ Vec.of(0,0,0), Vec.of(1,0,0), Vec.of(0,1,0)];   // Describe the where the points of a triangle are in space.
+      this.colors    = [ Color.of(1,0,0,1), Color.of(0,1,0,1), Color.of(0,0,1,1)];   // Besides a position, vertices also have a color.
       this.indexed   = false;                // With this turned off, every three vertices will be interpreted as one triangle.
     }
 }
